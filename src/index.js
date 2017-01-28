80d97be9776fd928f25e74fe932c6e150b2ee5cc
Nima Mehanian <nima.mehanian@gmail.com>
2017-01-28 14:23:08 -0800

    Get Headflow app up and running

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Root from './root';
+import store from './store';
+import './styles/manifest.styl';
+
+render(
+  <Root store={store} />,
+  document.getElementById('app'),
+);
---
+++ webpack.config.babel.js
@@ -0,0 +1,33 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const config = {
+  entry: [
+    (process.env.NODE_ENV === 'production' ? null :
+    'webpack-dev-server/client?http://localhost:8080'),
+    './src/index',
+  ].filter(entry => !!entry),
+  module: {
+    loaders: [
+      { test: /\.(js(x)?$)/, exclude: /node_modules/, loader: 'react-hot!babel' },
+      { test: /\.styl$/, exclude: /node_modules/, loader: 'style!css!stylus' },
+      // { test: /\.css$/, exclude: /node_modules/, loader: 'style!css' },
+      {
+        test: /\.(gif|jpg|png|svg|eot|otf|ttf|woff(2)?)$/,
+        exclude: /node_modules/,
+        loader: 'url?limit=100000&name=/[name].[ext]',
+      },
+    ],
+  },
+  output: { path: path.join(__dirname, '/dist'), filename: 'bundle.js' },
+  devServer: {
+    contentBase: 'dist',
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devtool: 'source-map',
+};
+
+export default config;
