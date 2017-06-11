e35700806cdaf0f87853993dbaa9e4cdc2224b62
Nima Mehanian <nima.mehanian@gmail.com>
2017-06-11 00:25:54 -0700

    Factor out Firebase project name

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase';
 import {
   FIREBASE_API_KEY,
-  FB_MSG_SEND_ID
+  FB_MSG_SEND_ID,
+  FB_DOMAIN_NAME
 } from '../keys';
 
 firebase.initializeApp({
   apiKey: FIREBASE_API_KEY,
-  authDomain: 'headflow-fde08.firebaseapp.com',
-  databaseURL: 'https://headflow-fde08.firebaseio.com',
+  authDomain: `${FB_DOMAIN_NAME}.firebaseapp.com`,
+  databaseURL: `https://${FB_DOMAIN_NAME}.firebaseio.com`,
   messagingSenderId: FB_MSG_SEND_ID,
-  storageBucket: 'headflow-fde08.appspot.com',
+  storageBucket: `${FB_DOMAIN_NAME}.appspot.com`,
 });
 
 export const DB = firebase.database();
